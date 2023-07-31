147a9a52c2758d1bcdf25915e75e5b06a8f0fd88
Martim Lobao <m@rtimlobao.com>
2023-07-31 09:02:32 +0100

    reschedule class for 7 am (#15)
    - class at 7 am during august

--- regibox/__main__.py
+++ regibox/__main__.py
@@ -3,9 +3,9 @@ import sys
 from regibox.regibox import LOGGER, main
 
 if __name__ == "__main__":
-    class_date, class_time = sys.argv[1:]
+    class_date, class_time, class_type = sys.argv[1:]  # pylint: disable=unbalanced-tuple-unpacking
     try:
-        main(class_date, class_time)
+        main(class_date, class_time, class_type)
     except RuntimeError as e:
         LOGGER.error(e)
         sys.exit(1)
