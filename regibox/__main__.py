826663d0fab42e308564180a9543234f483b4344
Martim Lobao <m@rtimlobao.com>
2023-09-26 12:41:28 +0100

    add english support (#20)
    - add english support
    - arg cleanup

--- regibox/__main__.py
+++ regibox/__main__.py
@@ -5,7 +5,7 @@ from regibox.regibox import LOGGER, main
 if __name__ == "__main__":
     class_date, class_time, class_type = sys.argv[1:]  # pylint: disable=unbalanced-tuple-unpacking
     try:
-        main(class_date, class_time, class_type)
+        main(class_date=class_date, class_time=class_time, class_type=class_type)
     except RuntimeError as e:
         LOGGER.error(e)
         sys.exit(1)
