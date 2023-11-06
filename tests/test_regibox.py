49e5c9c47bdc74f296733efb3c44ac0bc8bac262
Martim Lobao <m@rtimlobao.com>
2023-11-06 23:45:37 +0000

    use pants - use pants 2.17.0 - downgrade to python 3.11

--- tests/test_regibox.py
+++ tests/test_regibox.py
@@ -1,9 +1,7 @@
-import importlib
 import tomllib
 from pathlib import Path
 from typing import TYPE_CHECKING, Any
 
-from regibox import __package__ as main_package
 from regibox import __version__
 from regibox.regibox import WaitTime
 
@@ -17,8 +15,6 @@ def test_version() -> None:
         project_meta: dict[str, Any] = tomllib.load(file_)
 
     assert __version__ == project_meta["tool"]["poetry"]["version"]
-    # run `poetry install` if this is failing locally
-    assert __version__ == importlib.metadata.version(main_package)
 
 
 def test_range() -> None:
