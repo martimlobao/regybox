ba6bf9b7b16d75199a3fbee7e156f1760d2c1e68
Martim Lobao <m@rtimlobao.com>
2024-02-24 05:46:37 -0800

    fix bug in check_cal (#56)
    - fix bug in main function when passing in an argument to check_cal
    - add test

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ This is designed to run as a standalone app that runs on a schedule through GH a
 
 The app requires the following environment variables to be set:
 
-- `REGIBOX_USER`: The value for `regybox_user` in the [regybox.pt](https://regybox.pt) cookie
-- `PHPSESSID`: The value for `PHPSESSID` in the [regybox.pt](https://regybox.pt) cookie
+- `REGIBOX_USER`: The value for `regybox_user` in the [regybox.pt](https://www.regybox.pt/app/app_nova/index.php) cookie
+- `PHPSESSID`: The value for `PHPSESSID` in the [regybox.pt](https://www.regybox.pt/app/app_nova/index.php) cookie
 - `CALENDAR_URL`: The .ics URL of the calendar to use for the enrollment
 
 The calendar URL is optional, and is used to check if the user's personal calendar has a CrossFit class planned at the time of the enrollment. If it _does not_, the enrollment is skipped. The calendar must either be public or the URL must be accessible without authentication using the _"Secret address in iCal format"_ provided in Google Calendar.
---
+++ tests/test_calendar.py
@@ -0,0 +1,34 @@
+import datetime
+from zoneinfo import ZoneInfo
+
+import pytest
+
+from regybox.calendar import Calendar, check_cal
+from regybox.common import TIMEZONE
+from regybox.exceptions import UnplannedClassError
+
+
+def test_check_cal() -> None:
+    assert check_cal(datetime.date(2012, 2, 13), datetime.time(10, 0)) is True
+    with pytest.raises(UnplannedClassError):
+        check_cal(datetime.date(2012, 2, 13), datetime.time(10, 0), event_name="foo")
+
+    calendar = Calendar()
+    timezone = ZoneInfo("Europe/Vaduz")
+    assert calendar.find(when=datetime.datetime(2012, 2, 13, 10, 0, tzinfo=timezone)) is not None
+    assert calendar.find(when=datetime.datetime(2012, 2, 13, 9, 0, tzinfo=timezone)) is None
+    assert calendar.find(when=datetime.datetime(2012, 2, 13, 10, 0, tzinfo=TIMEZONE)) is None
+    assert calendar.find(when=datetime.datetime(2012, 2, 13, 9, 0, tzinfo=TIMEZONE)) is not None
+    assert (
+        calendar.find(
+            when=datetime.datetime(2012, 2, 13, 10, 0, tzinfo=timezone), event_name="foo"
+        )
+        is None
+    )
+    assert (
+        calendar.interval(
+            datetime.datetime(2012, 2, 13, 10, 0, tzinfo=timezone),
+            datetime.datetime(2012, 2, 13, 11, 0, tzinfo=timezone),
+        )
+        != []
+    )
