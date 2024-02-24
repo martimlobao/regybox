cab2f6c7a9927cbbc551fed0bb87fb53b4704608
Martim Lobao <m@rtimlobao.com>
2024-02-24 05:36:33 -0800

    fmt

--- tests/test_calendar.py
+++ tests/test_calendar.py
@@ -3,20 +3,30 @@ import datetime
 import pytest
 
 from regybox.calendar import Calendar, check_cal
+from regybox.common import TIMEZONE
 from regybox.exceptions import UnplannedClassError
 
 
 def test_check_cal() -> None:
-    assert check_cal(datetime.date(2012, 2, 13), datetime.time(10, 0), event_name=None) == True
+    assert check_cal(datetime.date(2012, 2, 13), datetime.time(10, 0)) is True
     with pytest.raises(UnplannedClassError):
         check_cal(datetime.date(2012, 2, 13), datetime.time(10, 0), event_name="foo")
 
     calendar = Calendar()
-    assert calendar.find(when=datetime.datetime(2012, 2, 13, 10, 0), event_name=None) is not None
-    assert calendar.find(when=datetime.datetime(2012, 2, 13, 10, 0), event_name="foo") == None
+    assert (
+        calendar.find(when=datetime.datetime(2012, 2, 13, 10, 0, tzinfo=TIMEZONE), event_name=None)
+        is not None
+    )
+    assert (
+        calendar.find(
+            when=datetime.datetime(2012, 2, 13, 10, 0, tzinfo=TIMEZONE), event_name="foo"
+        )
+        is None
+    )
     assert (
         calendar.interval(
-            datetime.datetime(2012, 2, 13, 10, 0), datetime.datetime(2012, 2, 13, 11, 0)
+            datetime.datetime(2012, 2, 13, 10, 0, tzinfo=TIMEZONE),
+            datetime.datetime(2012, 2, 13, 11, 0, tzinfo=TIMEZONE),
         )
         != []
     )
